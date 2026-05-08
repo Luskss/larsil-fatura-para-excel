35a0189e8a1f3bfc48029729f2ad167a264cad64
Luskss <lucasgabrielbarreto1231@gmail.com>
2026-05-08 16:29:30 -0300

    Use unsafe-inline in CSP to eliminate all hash errors permanently

--- api/usuarios.php
+++ api/usuarios.php
@@ -17,7 +17,7 @@ header('X-Content-Type-Options: nosniff');
 header('X-Frame-Options: DENY');
 header('X-XSS-Protection: 1; mode=block');
 header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
-header('Content-Security-Policy: default-src \'none\'; script-src \'self\'; style-src \'self\' fonts.googleapis.com');
+header('Content-Security-Policy: default-src \'self\'; script-src \'self\' \'unsafe-inline\'; style-src \'self\' \'unsafe-inline\' fonts.googleapis.com');
 header('Referrer-Policy: strict-origin-when-cross-origin');
 header('Permissions-Policy: camera=(), microphone=(), geolocation=()');
 
